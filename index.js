39b1e555db011b3961c398042da305e04f6600de
namacksin <134587502+Namacksin1234@users.noreply.github.com>
2023-06-04 17:48:01 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const event_1 = require("bdsx/event");
 event_1.events.serverOpen.on(() => {
-    Promise.resolve().then(() => require('./nms'));
-    console.log(`시간 플러그인이 작동되었습니다`);
+    console.log("시뮬 플러그인이 작동되었습니다");
+    Promise.resolve().then(() => require("./nms"));
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHNDQUFvQztBQUVwQyxjQUFNLENBQUMsVUFBVSxDQUFDLEVBQUUsQ0FBQyxHQUFHLEVBQUU7SUFDdEIscUNBQU8sT0FBTyxHQUFFO0lBQ2hCLE9BQU8sQ0FBQyxHQUFHLENBQUMsa0JBQWtCLENBQUMsQ0FBQztBQUNwQyxDQUFDLENBQUMsQ0FBQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHNDQUFvQztBQUVwQyxjQUFNLENBQUMsVUFBVSxDQUFDLEVBQUUsQ0FBQyxHQUFHLEVBQUU7SUFDdEIsT0FBTyxDQUFDLEdBQUcsQ0FBQyxrQkFBa0IsQ0FBQyxDQUFDO0lBQ2hDLHFDQUFPLE9BQU8sR0FBRTtBQUNwQixDQUFDLENBQUMsQ0FBQyJ9
--- nms.js
+++ nms.js
@@ -1,15 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const command_1 = require("bdsx/command");
-command_1.command.register("시간", "현재 시간을 알려줍니다").overload(async (param, origin) => {
+const command_1 = require("bdsx/bds/command");
+const player_1 = require("bdsx/bds/player");
+const command_2 = require("bdsx/command");
+const nativetype_1 = require("bdsx/nativetype");
+let counter = 0;
+command_2.command.register("시뮬레이트", "시뮬레이트 플레이어를 소환합니다", command_1.CommandPermissionLevel.Operator).overload((params, origin) => {
     const player = origin.getEntity();
-    if (player === null)
-        return;
-    let today = new Date();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
-    let milliseconds = today.getMilliseconds();
-    player.sendToastRequest(`${player.getName()}님, 현재 시간은`, `${hours}시 ${minutes}분 ${seconds}초 ${milliseconds}ms 입니다`);
-}, {});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibm1zLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibm1zLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsMENBQXVDO0FBRXZDLGlCQUFPLENBQUMsUUFBUSxDQUFDLElBQUksRUFBRSxjQUFjLENBQUMsQ0FBQyxRQUFRLENBQUUsS0FBSyxFQUFFLEtBQUssRUFBRSxNQUFNLEVBQUUsRUFBRTtJQUNyRSxNQUFNLE1BQU0sR0FBRyxNQUFNLENBQUMsU0FBUyxFQUFFLENBQUM7SUFDbEMsSUFBSSxNQUFNLEtBQUssSUFBSTtRQUFFLE9BQU87SUFDNUIsSUFBSSxLQUFLLEdBQUcsSUFBSSxJQUFJLEVBQUUsQ0FBQztJQUN2QixJQUFJLEtBQUssR0FBRyxLQUFLLENBQUMsUUFBUSxFQUFFLENBQUM7SUFDN0IsSUFBSSxPQUFPLEdBQUcsS0FBSyxDQUFDLFVBQVUsRUFBRSxDQUFDO0lBQ2pDLElBQUksT0FBTyxHQUFHLEtBQUssQ0FBQyxVQUFVLEVBQUUsQ0FBQztJQUNqQyxJQUFJLFlBQVksR0FBRyxLQUFLLENBQUMsZUFBZSxFQUFFLENBQUM7SUFDM0MsTUFBTSxDQUFDLGdCQUFnQixDQUFDLEdBQUcsTUFBTSxDQUFDLE9BQU8sRUFBRSxXQUFXLEVBQUUsR0FBRyxLQUFLLEtBQUssT0FBTyxLQUFLLE9BQU8sS0FBSyxZQUFZLFFBQVEsQ0FBQyxDQUFDO0FBQ3ZILENBQUMsRUFBQyxFQUFFLENBQUMsQ0FBQyJ9
+    if (player === null || player === void 0 ? void 0 : player.isPlayer) {
+        const name = params.name || "시뮬레이트 플레이어 " + ++counter;
+        const smltplayer = player_1.SimulatedPlayer.create(name, player.getPosition(), player.getDimensionId());
+    }
+    ;
+}, { name: [nativetype_1.CxxString, true] });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibm1zLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibm1zLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsOENBQTBEO0FBQzFELDRDQUFrRDtBQUNsRCwwQ0FBdUM7QUFDdkMsZ0RBQTRDO0FBRTVDLElBQUksT0FBTyxHQUFHLENBQUMsQ0FBQztBQUVoQixpQkFBTyxDQUFDLFFBQVEsQ0FBQyxPQUFPLEVBQUUsbUJBQW1CLEVBQUUsZ0NBQXNCLENBQUMsUUFBUSxDQUFDLENBQUMsUUFBUSxDQUNwRixDQUFDLE1BQU0sRUFBRSxNQUFNLEVBQUUsRUFBRTtJQUNmLE1BQU0sTUFBTSxHQUFHLE1BQU0sQ0FBQyxTQUFTLEVBQUUsQ0FBQztJQUNsQyxJQUFJLE1BQU0sYUFBTixNQUFNLHVCQUFOLE1BQU0sQ0FBRSxRQUFRLEVBQUU7UUFDbEIsTUFBTSxJQUFJLEdBQUcsTUFBTSxDQUFDLElBQUksSUFBSSxhQUFhLEdBQUcsRUFBRSxPQUFPLENBQUM7UUFDdEQsTUFBTSxVQUFVLEdBQUcsd0JBQWUsQ0FBQyxNQUFNLENBQUMsSUFBSSxFQUFFLE1BQU0sQ0FBQyxXQUFXLEVBQUUsRUFBRSxNQUFNLENBQUMsY0FBYyxFQUFFLENBQUMsQ0FBQztLQUNsRztJQUFBLENBQUM7QUFDTixDQUFDLEVBQ0QsRUFBRSxJQUFJLEVBQUUsQ0FBQyxzQkFBUyxFQUFFLElBQUksQ0FBQyxFQUFFLENBQzlCLENBQUMifQ==
